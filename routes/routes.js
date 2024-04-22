d1b974feaa4279ffb5d43faaf929c47510b61ae5
David Cárdenas <santito500@live.com>
2024-04-22 08:15:37 -0500

    fix: :ambulance: Cambio de método
    Cambio de método para /subasta/buzon con GET

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ const router = Router();
 router.post("/add-subastar", SubastarController.add_subasta);
 router.get("/get-cartas-subasta", SubastarController.get_cartasSubasta);
 // ?  <--BUZON-->
-router.post("/buzon", BuzonController.getData);
+router.get("/buzon", BuzonController.getData);
 router.post("/buzon/claim", BuzonController.claimAssets);
 router.post("/buzon/add", BuzonController.setData);
 
