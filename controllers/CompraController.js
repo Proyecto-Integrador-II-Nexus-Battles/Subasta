acb4d9550a25f15379f08bf1dd122dd62cafc4f9
Alexa020204 <alexa020204@gmail.com>
2024-04-23 22:45:39 -0500

    hhh

---
+++ controllers/CompraController.js
@@ -0,0 +1,70 @@
+
+import pool from "../models/BDconexion.js";
+import axios from "axios";
+
+export class CompraController {
+
+    static async enviarCompra(req, res) {
+        try {
+            const { IdUsuario, ID_SUBASTA } = req.body;
+
+            const ID_CARTA = await pool.query('SELECT ID_CARTA FROM CARTA_SUBASTA WHERE ID = ?', [ID_SUBASTA]);
+            console.log(ID_CARTA);
+            axios
+                .post("/subasta/buzon/add", {
+                    IdUsuario: IdUsuario,
+                    carta: [{ ID_CARTA: ID_CARTA[0].ID_CARTA, CANTIDAD: 1 }],
+                })
+                .then(() => {
+                    console.log("Carta del ganador agregada al buzon correctamente");
+                    axios.get(`/subasta/getSubasta/${ID_SUBASTA}`)
+                        .then((res) => {
+                            console.log("si entro")
+                            const datos = res.data;
+                            axios
+                                .post("/subasta/buzon/add", {
+                                    IdUsuario: datos[0].ID_USUARIO,
+                                    creditos: datos[0].CREDITOS_MAX,
+                                })
+                                .then(() => {
+                                    console.log("Creditos agregados al buzon correctamente del subastador");
+                                })
+                                .catch((error) => {
+                                    console.error("Error al agregar los creditos al buzon del subastador:", error);
+                                });
+                            if (datos[0].CARTAS_MAX.length > 0) {
+                                datos[0].CARTAS_MAX.forEach((carta) => {
+                                    axios
+                                        .post("/subasta/buzon/add", {
+                                            IdUsuario: datos[0].ID_USUARIO,
+                                            carta: [{ ID_CARTA: carta.ID, CANTIDAD: carta.CANTIDAD }]
+                                        })
+                                        .then(() => {
+                                            console.log("Carta agregada al buzon correctamente Maximas");
+                                        })
+                                        .catch((error) => {
+                                            console.error("Error al agregar carta al buzon:", error);
+                                        });
+                                });
+                            }   
+                        })
+                .catch((error) => {
+                    console.error("Error al agregar carta al buzon:", error);
+                });
+            });
+            axios.get(`/subasta/deleteSubasta/${ID_SUBASTA}`)
+                .then(() => {
+                    console.log("Subasta eliminada correctamente");
+                    res.status(200).send("Compra realizada correctamente");
+                })
+                .catch((error) => {
+                    console.error("Error al eliminar la subasta:", error);
+                });
+
+        } catch (error) {
+            // Manejo de errores
+            console.error("Error en la función enviarCompra:", error);
+            res.status(500).send("Error en el servidor");
+        }
+    }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { SubastarController } from "../controllers/SubastarController.js";
 import { BuzonController } from "../controllers/BuzonController.js";
 import { PujarController } from "../controllers/PujaController.js";
+import { CompraController } from "../controllers/CompraController.js";
 
 const router = Router();
 
@@ -10,6 +11,7 @@ router.get("/get-cartas-subasta", SubastarController.get_cartasSubasta);
 router.get("/getSubasta/:idSubasta", SubastarController.getSubasta);
 router.post("/addPuja", PujarController.add_puja);
 router.get("/deleteSubasta/:idSubasta", SubastarController.deleteSubasta);
+router.post("/compraRapida", CompraController.enviarCompra);
 // ?  <--BUZON-->
 router.get("/buzon", BuzonController.getData);
 router.post("/buzon/claim", BuzonController.claimAssets);
